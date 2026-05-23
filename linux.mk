fda33a92f02845b37f4fca2042de84d14f9020bb
qqluqq <pieter00@gmail.com>
2026-05-23 15:36:05 +0200

    use lib

--- linux.mk
+++ linux.mk
@@ -293,6 +293,7 @@ KFX_CXXFLAGS += -g -DDEBUG -DBFDEBUG_LEVEL=0 -O3 -march=x86-64 $(KFX_INCLUDES) -
 KFX_LDFLAGS += \
 	-g \
 	-rdynamic \
+	-Wl,-rpath,'$$ORIGIN/lib:$$ORIGIN' \
 	-Wall -Wextra -Werror \
 	-Ldeps/astronomy -lastronomy \
 	-Ldeps/centijson -ljson \
